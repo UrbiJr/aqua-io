71f4dff855ede0e6f95285d80dd847284d5f4fc4
Federico Urbinelli <federico.urbinelli@gmail.com>
2022-12-05 19:50:14 +0100

    added startup greetings, added init function

--- cmd/cactus/main.go
+++ cmd/cactus/main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
+	"time"
 
+	"github.com/cactus-aio/go-cactus/internal/user"
 	"github.com/cactus-aio/go-cactus/internal/utils"
 )
 
-// app entry point
-func main() {
+func init() {
+	rand.Seed(time.Now().Unix())
 
 	debugArg := flag.Bool("debug", false, "enable debug mode") // go run ./cmd/cactus/main.go -debug
 	flag.Parse()
@@ -17,8 +20,31 @@ func main() {
 
 	utils.SetDebug(debug)
 	go utils.BlockNetworkSniffing()
+}
+
+// app entry point
+func main() {
 
 	log.Println("Booting up...")
+
+	user := user.NewUser(
+		"LICENSE-KEY",
+		"example@gmail.com",
+		"",
+		"cactus-user",
+	)
+
+	greetings := []string{
+		"how can Cactus-AIO assist you today? :-)",
+		"how are you going to use Cactus-AIO today? :-)",
+		"ready to have some fun with Cactus-AIO?",
+		"Cactus-AIO is at your service :cactus-salute:",
+		"it's been a while.",
+		"time to make the success log go brrr.",
+		"everyday is a perfect day to run Cactus-AIO!",
+	}
+
+	log.Printf("Hello %s, %s", user.Username, greetings[rand.Intn(len(greetings))])
 
 	fmt.Printf("Press <ENTER> to exit...")
 	fmt.Scanln() // wait for Enter Key
